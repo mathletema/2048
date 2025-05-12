c918a2a87cda8a76c5e29d207d23cee2bf96feb3
Ishank Agrawal <agr.ishank@gmail.com>
2025-05-12 00:59:55 -0400

    minor edits

--- include/display.h
+++ include/display.h
@@ -4,7 +4,6 @@
 #include "constants.h"
 #include "color.h"
 
-
 uint8_t displayInit();
 void displayBlitzRGB(color_t* buffer);
 void displayQuit();
--- src/main.c
+++ src/main.c
@@ -12,30 +12,28 @@ int main() {
     color_t arr[DISPLAY_WIDTH * DISPLAY_HEIGHT];
 
     int board[] = {
-        4, 2, 2, 0,
-        0, 0, 2, 0,
-        0, 0, 0, 2,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
         0, 0, 0, 0
     };
-
-    drawBoard(arr, board);
-    displayBlitzRGB(arr);
+    addRandomTile(board);
+    addRandomTile(board);
     
 
     uint8_t running = 1;
     while (running) {
+        drawBoard(arr, board);
+        displayBlitzRGB(arr);
+
         input_t inp = pollInput();
-        printf("recvd input!!\n");
         if (inp == INPUT_QUIT) {
-            printf("input was quit!\n");
             displayQuit();
             running = 0;
-        } else {
-            if (inp == INPUT_KEY_UP) moveUp(board);
-            else if (inp == INPUT_KEY_DOWN) moveDown(board);
-            else if (inp == INPUT_KEY_LEFT) moveLeft(board);
-            else if (inp == INPUT_KEY_RIGHT) moveRight(board);
-        }
+        } else if (inp == INPUT_KEY_UP) moveUp(board);
+        else if (inp == INPUT_KEY_DOWN) moveDown(board);
+        else if (inp == INPUT_KEY_LEFT) moveLeft(board);
+        else if (inp == INPUT_KEY_RIGHT) moveRight(board);
 
         drawBoard(arr, board);
         displayBlitzRGB(arr);
